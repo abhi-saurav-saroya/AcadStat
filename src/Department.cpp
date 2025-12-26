cd792820448235032f2222a36f5e78a6c9d67406
abhi-saurav-saroya <abhisauravsaroya06@gmail.com>
2025-12-26 14:31:08 +0530

    Department.cpp

---
+++ src/Department.cpp
@@ -0,0 +1,46 @@
+#include "C:\Users\hp\OneDrive\Documents\PROJECTS\Fun Activities\AcadStat\include\Department.h"
+#include "C:\Users\hp\OneDrive\Documents\PROJECTS\Fun Activities\AcadStat\include\Section.h"
+#include "C:\Users\hp\OneDrive\Documents\PROJECTS\Fun Activities\AcadStat\include\utils.h"
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <limits>
+#include <cctype>
+using namespace std;
+
+Department:: Department(string name) : departmentName(name) {}
+        
+string Department::getDepartmentName() const {
+    return departmentName;
+}
+
+int Department::displaySections() const {
+    if(sections.empty()) {
+        cout << "No sections available in this department." << endl;
+        return 0;
+    }
+    
+    cout << "Section List:" << endl;
+    for (size_t i = 0; i < sections.size(); ++i) {
+        cout << "\t" << (i + 1) << ". " << sections[i].getSectionName() << endl;
+    }
+    return static_cast<int>(sections.size());
+}
+
+bool Department::sectionExists(const string& secName) const {
+    for (const Section& sec : sections) {
+        if (toLower(sec.getSectionName()) == toLower(secName))
+            return true;
+    }
+    return false;
+}
+
+Section& Department::getSectionByIndex(int index) {
+    return sections[index];
+}
+
+void Department::addSection(const Section& sec) {
+    sections.push_back(sec);
+}
+
