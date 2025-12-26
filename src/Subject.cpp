abc42b4b1da3b87b6733c95fc19ea01cbb4c906f
abhi-saurav-saroya <abhisauravsaroya06@gmail.com>
2025-12-26 14:32:27 +0530

    Subject.cpp

---
+++ src/Subject.cpp
@@ -0,0 +1,31 @@
+#include "C:\Users\hp\OneDrive\Documents\PROJECTS\Fun Activities\AcadStat\include\Subject.h"
+#include "C:\Users\hp\OneDrive\Documents\PROJECTS\Fun Activities\AcadStat\include\utils.h"
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cctype>
+using namespace std;
+
+Subject:: Subject(string name, string code, int mcode, float marks) : 
+                subjectName(name), subjectCode(code), MCode(mcode), marksObtained(marks) {}
+
+string Subject::getSubjectCode() const {
+    return subjectCode;
+}
+
+int Subject::getMCode() const {
+    return MCode;
+}
+
+string Subject::getSubjectName() const {
+    return subjectName;
+}
+
+float Subject::getMarks() const {
+    return marksObtained;
+}
+
+void Subject::setMarks(float marks) {
+    marksObtained = marks;
+}
