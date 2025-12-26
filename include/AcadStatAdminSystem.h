73e7d777e6cb5d012b7144d9defe8e632caeaa23
abhi-saurav-saroya <abhisauravsaroya06@gmail.com>
2025-12-26 14:19:49 +0530

    AcadStatAdminSystem.h

---
+++ include/AcadStatAdminSystem.h
@@ -0,0 +1,30 @@
+#ifndef ACADSTATADMINSYSTEM_H
+#define ACADSTATADMINSYSTEM_H
+
+#include <vector>
+#include <string>
+#include <cctype>
+using namespace std;
+
+#include "Department.h"
+
+class AcadStatSystem {
+    private:
+        string PassKey;
+        vector<Department> departments;
+
+    public:
+        AcadStatSystem();
+        bool adminLogin(const string& key);
+        void mainMenu();
+        void addDepartment();
+        void addSection();
+        void addStudent();
+        void manipulateData();
+        void addSubjects();
+        void addMarks();
+        void showReports();
+        int displayDepartments() const;
+};
+
+#endif
